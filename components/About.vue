7c183a7e6f1ed037e7efcb14653e89e75b02a8de
Juul van Tilburg <juulvantilburg@gmail.com>
2023-10-04 22:13:51 +0200

    Index page

--- components/About.vue
+++ components/About.vue
@@ -1,21 +1,28 @@
 <template>
-        <div class="flex flex-col gap-3 items-center justify-between items-center bg-primary relative">
-            <div class="flex flex-row items-center flex-wrap justify-between gap-8">
-                <div>Wat is Technovium Unlimited</div>
-                <div></div>
-            </div>
-            <div class="flex flex-row gap-8 md:gap-12">
-                <div class="p-4 rounded-lg bg-bgsecondary flex flex-col gap-3">
-                    <div class="text-lg">Continue groei</div>
-                    <div class="text-light">Door continue nieuwe input vanuit docenten, studenten en werkveld te integreren blijft kennis up-to-date</div>
+        <div class="container mx-auto mt-12 md:mt-24">
+            <div class="flex flex-col gap-8 bg-primary relative p-8 rounded-xl">
+                <div class="flex flex-row items-center flex-wrap justify-between gap-8">
+                    <div class="flex flex-row gap-3">
+                        <div class="font-black"><div><span class="md:text-xl">Wat is </span><span class="text-xl md:text-4xl">Technovium</span></div><div><span class="text-4xl md:text-6xl">Unlimited</span></div></div>
+                        <div class="text-6xl md:text-8xl font-black"><i class="fa-solid fa-question"></i></div>
+                    </div>
+                    <a href="/about" class="hidden md:flex flex-row items-center justify-center gap-3 h-8 w-28 lg:h-12 lg:w-36 border-2 border-bgbuttonprimary font-bold hover:opacity-70 rounded-xl uppercase">
+                        Lees meer
+                    </a>
                 </div>
-                <div class="p-4 rounded-lg bg-bgsecondary flex flex-col gap-3">
-                    <div class="text-lg">Continue groei</div>
-                    <div class="text-light">Door continue nieuwe input vanuit docenten, studenten en werkveld te integreren blijft kennis up-to-date</div>
-                </div>
-                <div class="p-4 rounded-lg bg-bgsecondary flex flex-col gap-3">
-                    <div class="text-lg">Continue groei</div>
-                    <div class="text-light">Door continue nieuwe input vanuit docenten, studenten en werkveld te integreren blijft kennis up-to-date</div>
+                <div class="flex flex-row gap-8 md:gap-12 flex-wrap">
+                    <div class="flex-1 basis-72 p-6 rounded-xl bg-bgsecondary flex flex-col gap-3">
+                        <div class="text-lg font-black">Continue groei</div>
+                        <div class="text-light font-medium">Door continue nieuwe input vanuit docenten, studenten en werkveld te integreren blijft kennis up-to-date</div>
+                    </div>
+                    <div class="flex-1 basis-72 p-6 rounded-xl bg-bgsecondary flex flex-col gap-3">
+                        <div class="text-lg font-black">Ruimte voor talent</div>
+                        <div class="text-light font-medium">Er is binnen Technovium Unlimited ruimte voor iedere discipline en talent. Iedereen kan deelnemen aan Technovium Unlimited, zowel als gebruiker of ontwikkelaar</div>
+                    </div>
+                    <div class="flex-1 basis-72 p-6 rounded-xl bg-bgsecondary flex flex-col gap-3">
+                        <div class="text-lg font-black">Continue groei</div>
+                        <div class="text-light font-medium">Door continue nieuwe input vanuit docenten, studenten en werkveld te integreren blijft kennis up-to-date</div>
+                    </div>
                 </div>
             </div>
         </div>
