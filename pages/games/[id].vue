0ef2c249def0dab4766a3072dd468a4bd91f100c
Juul van Tilburg <juulvantilburg@gmail.com>
2023-10-07 22:54:23 +0200

    game

--- pages/games/[id].vue
+++ pages/games/[id].vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="slide-wrapper">
-    <div class="container-fluid">
-      <div class="row justify-content-center">
-
-        <div id="game" style="width: 960px; height: 600px; border: 1px solid #fff"  >
-          <iframe  v-if="embeddedGameUrl"
-            width="100%"
-            height="600"
+  <div class="container mx-auto mt-12 md:mt-24">
+    <div class="flex flex-row gap-8 flex-wrap">
+      <div class="flex-1 w-fit basis-2/3 bg-bgsecondary px-4 py-6 rounded-lg shadow-md">
+        <div class="w-full aspect-w-16 aspect-h-9 h-[642px]" id="game" >
+          <iframe class="w-full h-full" v-if="embeddedGameUrl"
             :src="embeddedGameUrl"
-            frameborder="0"
-            allowfullscreen
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
           ></iframe>
         </div>
+      </div>
+      <div class="flex-1 basis-96 bg-bgsecondary rounded-lg shadow-md flex flex-col gap-3 p-8">
+        <h1 class="font-black text-3xl">ROC NIJMEGEN MAZE</h1>
+        <div class="flex flex-row items-center gap-5 font-black text-light text-lg">
+          <span class="">Version 1.7.9</span>
+          <span class="">2023</span>
+        </div>
+        <div class="flex flex-row gap-3 items-center font-semibold text-sm group-first:text-base" style="color:crimson"><div class="h-2 w-2" style="background-color: crimson;"></div><div>3D omgeving</div></div>
+        <p class="text-light whitespace-break-spaces">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et 
+dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
       </div>
     </div>
   </div>
