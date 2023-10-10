5bc3e160de7b8f04e87f099b406620541a495233
Juul van Tilburg <juulvantilburg@gmail.com>
2023-10-10 19:27:55 +0200

    Image for card

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <a :href="card.link" class="flex flex-col bg-bgsecondary rounded-xl overflow-hidden hover:-translate-y-3 transition duration-300 ease-in-out">
     <div class="gradient-top relative h-44 flex-none">
-        <img :src="card._thumb" alt="Card Image" class="max-h-full min-w-full object-cover h-44 object-center">
+        <img :src="card._thumb[0]" alt="Card Image" class="max-h-full min-w-full object-cover h-44 object-center">
     </div>
     <div class="p-6 flex flex-col gap-1 flex-1 justify-center">
         <h3 class="text-base font-black">{{ card.name }}</h3>
