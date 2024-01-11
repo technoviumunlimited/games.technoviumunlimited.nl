7a762c9faac726163374e2f7e850eb61ccb69e3a
IrisReijnen <ir.g.reijnen@gmail.com>
2024-01-11 08:44:56 +0100

    start

---
+++ components/HallOfFameCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <a>
+        <img src="" alt="">
+        <div>
+            <div>Iris Reijnen</div>
+            <div>Lead Developer</div>
+            <div>Software Developer</div>
+        </div>
+    </a>
+</template>
+
+<script>
+export default {
+  props: {
+    card: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ pages/hall_of_fame/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="container mx-auto mt-24 font-black text-3xl uppercase">Hall of Fame</div>
+        <div>
+            <div>
+                <div>Frontend</div>
+            </div>
+            <div class="flex-1 basis-full flex flex-col items-center gap-8">
+                <HallOfFameCardList />
+            </div>
+        </div>
+    </div>
+</template>
